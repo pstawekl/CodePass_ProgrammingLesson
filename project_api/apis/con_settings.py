3af468ec86836ab99a5f52f0dfb5d28fdd879331
Jakub Stawski <51178880+pstawekl@users.noreply.github.com>
2023-11-16 23:02:25 +0100

    Poprawa ścieżki do config.json

--- project_api/apis/con_settings.py
+++ project_api/apis/con_settings.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 class con_settings():
     def __init__(self):
-        self.config_path = (f'{Path( __file__ ).parent.parent.parent.parent.absolute()}'+'/config.json').replace('\\', '/')
+        self.config_path = (f'{Path( __file__ ).parent.parent.parent.absolute()}'+'/config.json').replace('\\', '/')
